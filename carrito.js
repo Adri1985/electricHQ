20d62d8870e0dc875c2154d8b04680f2cf964ee4
Adrian Campo <adrian.campo1iclodud.com>
2022-07-08 15:53:11 -0300

    avanzando proceso check out y proyecto final

---
+++ carrito.js
@@ -0,0 +1,31 @@
+function mostrarCarrito()
+{   
+        console.log("entra en mostrar carrito "+carritoArray.length);
+        let carritoDom = document.getElementById("carrito");
+        carritoDom.innerHTML = "";
+        let cant=document.getElementById("cantidad");
+        cant.innerHTML="";
+        
+        if(carritoArray.length>0)
+        {
+            console.log("entra en >0");
+            cant.innerHTML=`<p style="color:orange;">${carritoArray.length}</p>`
+        }
+        
+
+       
+        let carro=document.createElement("img");
+            carro.src="../images/cart.png"
+            carro.addEventListener('click',()=>{
+                if(carritoArray.length>0){
+                    finalizarCompra();
+                }else
+                {
+                    Swal.fire("Cart is empty");
+                }
+
+                
+            });
+            carritoDom.appendChild(carro);
+            
+}
--- productos.js
+++ productos.js
@@ -326,45 +326,6 @@ function buscar() {
 
 }
 
-function mostrarCarrito()
-{   
-        console.log("entra en mostrar carrito "+carritoArray.length);
-        let carritoDom = document.getElementById("carrito");
-        carritoDom.innerHTML = "";
-        let cant=document.getElementById("cantidad");
-        cant.innerHTML="";
-        
-        if(carritoArray.length>0)
-        {
-            console.log("entra en >0");
-            cant.innerHTML=`<p style="color:orange;">${carritoArray.length}</p>`
-        }
-        
-
-       
-        let carro=document.createElement("img");
-            carro.src="../images/cart.png"
-            carro.addEventListener('click',()=>{
-                if(carritoArray.length>0){
-                    finalizarCompra();
-                }else
-                {
-                    Swal.fire("Cart is empty");
-                }
-
-                
-            });
-            carritoDom.appendChild(carro);
-            
-
-            
-        /*ejecucion != 1 ? localStorage.setItem("listacarr", JSON.stringify(carrito)) : "";
-        carrito.forEach(auto => { document.getElementById("btnrmv" + auto.id).addEventListener("click", function () { sacarCarrito(auto) }) });*/
-    
-    
-    }
-
-
 function listProducts(lista) {
 
  
@@ -697,7 +658,7 @@ function finalizarCompra() {
                     <button type="button" id="fin" class="btn btn-warning btn-block btn-lg">Buy now</button>
   
                     <h5 class="fw-bold mb-5" style="position: absolute; bottom: 0;">
-                      <a href="products.html" id="continueShopping"><i class="fas fa-angle-left me-2"></i>Back to shopping</a>
+                      <a href="/pages/products.html" id="continueShopping"><i class="fas fa-angle-left me-2"></i>Back to shopping</a>
                     </h5>`
     // sigue a la par del div 7 
 
